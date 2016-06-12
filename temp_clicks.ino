491272e706c9013e2fae3aa4aa9f77a92e508c9c
Octávio Richter <octaviorichter@gmail.com>
2016-06-12 12:26:53 -0300

    Sketch that reads temperature & sensor clicks

---
+++ temp_clicks.ino
@@ -0,0 +1,36 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#define ONE_WIRE_BUS 7
+volatile int count;
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature.
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(2, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(2), click, RISING);
+
+  Serial.begin(9600);
+  sensors.begin();
+}
+
+void loop() {
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  Serial.print("Temp: ");
+  Serial.print(sensors.getTempCByIndex(0));
+
+  Serial.print(" | Clicks: ");
+  Serial.println(count);
+
+  count = 0;
+
+  delay(5000);
+}
+
+void click() {
+  count++;
+}
