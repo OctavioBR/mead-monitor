6af1ff417ef038aac679108ccacb6e6c221e9bdf
Octávio Richter <octaviorichter@gmail.com>
2016-06-13 02:17:51 -0300

    Adds plotly support for serial script

--- arduino_serial.py
+++ arduino_serial.py
@@ -24,7 +24,55 @@ def save_on_file(arduino_serial, file_path):
 		pass
 
 def plotly_stream(arduino_serial):
-	pass
+	username = 'OctavioBR'
+	api_key = 't9qlzlod0v'
+	stream_token1 = 'yyml0x0ycp'
+	stream_token2 = 'xve4lhin3t'
+	py.sign_in(username, api_key)
+
+	trace1 = Scatter(
+		x=[], y=[],
+		name="temperature",
+		stream=dict(token=stream_token1, maxpoints=80)
+	)
+
+	trace2 = Scatter(
+		x=[], y=[],
+		name="flow_clicks", yaxis="y2",
+		stream=dict(token=stream_token2, maxpoints=80),
+		marker=dict(color='rgb(148, 103, 189)')
+	)
+
+	layout = Layout(
+		title="Temperature & flow clicks",
+		yaxis=dict(title="y for trace1"),
+		yaxis2=dict(
+			title="clicks scale",
+			titlefont=dict(color="rgb(148, 103, 189)"),
+			tickfont=dict(color="rgb(148, 103, 189)"),
+			overlaying="y",side="right"
+		)
+	)
+
+	fig = Figure(data=[trace1, trace2], layout=layout)
+	plot_url = py.plot(fig, filename="Temperature & Flow")
+
+	stream1 = py.Stream(stream_token1)
+	stream2 = py.Stream(stream_token2)
+	stream1.open()
+	stream2.open()
+
+	try:
+		while True:
+			x = datetime.datetime.now()
+			input = arduino_serial.readline()
+			stream1.write({"x": x, "y": input.split(",")[0]})
+			stream2.write({"x": x, "y": input.split(",")[1]})
+	except KeyboardInterrupt:
+		stream1.close()
+		stream2.close()
+		print "\nStopped plotting at " + plot_url
+		pass
 
 def main():
 	port = serial_port()
@@ -33,7 +81,8 @@ def main():
 	arduino_serial = serial.Serial(port, 9600)
 	arduino_serial.flushInput()
 
-	save_on_file(arduino_serial, "data.csv")
+	# save_on_file(arduino_serial, "data.csv")
+	plotly_stream(arduino_serial)
 
 if __name__ == "__main__":
     main()
